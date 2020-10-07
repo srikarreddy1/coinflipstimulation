4fd8deb269ff18b104b3a43c8f999f80363d03d2
unknown <fddxzf@gmail.com>
2020-10-08 01:14:14 +0530

    display who won by how much

--- coinflip.sh
+++ coinflip.sh
@@ -18,3 +18,22 @@
      echo "tails is the winner"
    fi
    done
+   # loop until one of them wins 21 times
+   declare -A coinflip
+   number=0
+   while (( coinflip[$number] != 21 ))
+   do
+    number=$((RANDOM%2))
+    if [[ $number -eq 1 ]]
+    then
+      coinflip[$number]=$((coinflip[$number]+1))
+    else
+     coinflip[$number]=$((coinflip[$number]+1))
+    fi
+   done
+   if [[ coinflip[1] -eq 21 ]]
+   then
+      echo "heads won over tails by $((${coinflip[1]}-${coinflip[0]}))"
+   else
+      echo "tails won over heads by $((${coinflip[0]}-${coinflip[1]}))"
+   fi
