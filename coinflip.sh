341a0ea77e5ae5b0e3c439ed4df5bb75495b777a
unknown <fddxzf@gmail.com>
2020-10-08 00:56:07 +0530

    loop through and show winner

--- coinflip.sh
+++ coinflip.sh
@@ -8,3 +8,13 @@
    else
      echo "tails is the winner"
    fi
+   #loop though coin filp and show winner
+   for (( i=0; i<=20; i++ ))
+   do
+     if [[ $((RANDOM%2)) -eq 1 ]]
+   then
+     echo "head's is the winner"
+   else
+     echo "tails is the winner"
+   fi
+   done
