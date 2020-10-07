999b14e51a2c3ddd880411b9872b17d174654bd6
unknown <fddxzf@gmail.com>
2020-10-08 00:50:21 +0530

    coin flip code to generate winner

--- coinflip.sh
+++ coinflip.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 
    echo "welcome to coin flip computation code"
+   #stimulate a coin flip
+   if [[ $((RANDOM%2)) -eq 1 ]]
+   then
+     echo "head's is the winner"
+   else
+     echo "tails is the winner"
+   fi
